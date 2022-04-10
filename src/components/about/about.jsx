1a04cc552e84245f8adae417c96fe8b36e0267ed
Emi <emohamadz2012@gmail.com>
2022-04-10 20:04:14 +0430

    about section done

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,10 +1,51 @@
-import React from 'react'
-import './about.css'
+import React from "react";
+import "./about.css";
+import ME from '../../asset/me-about.jpg'
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+
+import {VscFolderLibrary} from 'react-icons/vsc'
+
 
 const about = () => {
   return (
-    <section id='about'>about</section>
-  )
-}
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
 
-export default about
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Image" />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon"/>
+              <h5>Experience</h5>
+              <small>3+ Years Working</small>
+            </article>
+
+            <article className="about__card">
+              <FiUsers className="about__icon"/>
+              <h5>Clients</h5>
+              <small>200+ Worldwide</small>
+            </article>
+
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon"/>
+              <h5>Projects</h5>
+              <small>80+ Completed Projects</small>
+            </article>
+
+          </div>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas repudiandae vero libero ex ipsam deserunt sed nobis suscipit aliquid unde doloribus beatae voluptatibus aspernatur, ipsum rem animi maiores. Alias, exercitationem?</p>
+          <a href="#contact" className="btn btn-primary">Let's Talk</a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default about;
