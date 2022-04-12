fc73973e8bac53b492c15d38293dff501f4a74c7
Emi <emohamadz2012@gmail.com>
2022-04-13 00:51:08 +0430

    footer done

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,11 +1,52 @@
-import React from 'react'
+import React from "react";
 
-import './footer.css'
+import "./footer.css";
+
+import {FaFacebookF} from 'react-icons/fa';
+
+import {FiInstagram} from 'react-icons/fi';
+
+import {IoLogoTwitter} from 'react-icons/io';
 
 const footer = () => {
   return (
-    <section id='footer'>footer</section>
-  )
-}
+    <footer>
+      <a href="#" className="footer__logo">
+        Mamad
+      </a>
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#exprience">Exprience</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+      <div className="footer__socials">
+        <a href="https://facebook.com"><FaFacebookF/></a>
+        <a href="https://instagram.com"><FiInstagram/></a>
+        <a href="https://twitter.com"><IoLogoTwitter/></a>
+      </div>
+      <div className="footer__copyright">
+        <small>&copy;Mamad Tutorial .All Rights Reserved.</small>
+      </div>
+    </footer>
+  );
+};
 
-export default footer
+export default footer;
